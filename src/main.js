eaf8190b3b5bcb5be59faa9dbce082dec497c900
kevin940726 <kevin830726@gmail.com>
2016-05-16 00:49:33 +0800

    onblur changed to visibilitychange

--- src/main.js
+++ src/main.js
@@ -36,16 +36,35 @@ const history = syncHistoryWithStore(browserHistory, store, {
   selectLocationState: (state) => state.router
 });
 
-window.onblur = () => {
-  store.dispatch(controlStop());
-  store.dispatch(pause());
-  store.dispatch(controlSetPause());
-};
-window.onfocus = () => {
-  store.dispatch(controlStop());
-  store.dispatch(pause());
-  store.dispatch(controlSetPlay());
-};
+// ========================================================
+// Visibility event
+// ========================================================
+let hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.mozHidden !== "undefined") {
+  hidden = "mozHidden";
+  visibilityChange = "mozvisibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+document.addEventListener(visibilityChange, () => {
+  if (document[hidden]) {
+    store.dispatch(controlStop());
+    store.dispatch(pause());
+    store.dispatch(controlSetPause());
+  } else {
+    store.dispatch(controlStop());
+    store.dispatch(pause());
+    store.dispatch(controlSetPlay());
+  }
+}, false);
 
 // ========================================================
 // Firebase Setup
